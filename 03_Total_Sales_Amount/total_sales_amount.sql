2f12d4da2ff1d95a1e17cb4eee9eb337bef3dc9f
Nandini <nandinitirumalaraju555@gmail.com>
2026-06-17 00:43:27 +0530

    Add SQL query for total sales amount by date

---
+++ 03_Total_Sales_Amount/total_sales_amount.sql
@@ -0,0 +1,9 @@
+SELECT s.date,
+       SUM(g.gross_price*s.sold_quantity) AS gross_price_total
+FROM fact_sales_monthly s
+JOIN fact_gross_price g
+ON g.product_code = s.product_code
+AND g.fiscal_year = get_fiscal_year(s.date)
+WHERE customer_code = 90002002
+GROUP BY s.date
+ORDER BY s.date ASC;
